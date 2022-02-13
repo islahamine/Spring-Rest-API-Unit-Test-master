4ee1d90d7e378d748c2bd43ee85a409d3de3317b
islahamine <amineislah1997@gmail.com>
2022-02-13 01:02:57 +0100

    new modification

--- src/main/java/com/sundaydevblog/springrestapitest/controller/MemberController.java
+++ src/main/java/com/sundaydevblog/springrestapitest/controller/MemberController.java
@@ -72,7 +72,7 @@ public class MemberController {
 
     @GetMapping(value = "/hello")
     public String sayHi(){
-        return "Say hello to my little friend, Jenkinss!";
+        return "Say hello to my little friend, amine islah!";
     }
 
 }
